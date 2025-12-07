ec64461b170c730ee36a873f9bda6b7014e760f2
OppaAI <oppa.ai.org@gmail.com>
2025-12-06 18:27:38 -0800

    Remove the "node" in the fuction name

--- server/src/vsc/setup.py
+++ server/src/vsc/setup.py
@@ -24,7 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'heartbeat_node = vsc.heartbeat_node:main',
+            'heartbeat = vsc.heartbeat:main',
         ],
     },
 )
