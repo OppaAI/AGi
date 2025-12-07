a8c4f46a22ea808dd351380c19224a5feccd4d2a
OppaAI <oppa.ai.org@gmail.com>
2025-12-06 18:09:54 -0800

    Move server repo to server sub-direcrory

--- server/src/vsc/vsc/heartbeat.py
+++ server/src/vsc/vsc/heartbeat.py
@@ -2,24 +2,29 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+import sys
 
 RED = "\033[91m"
 RESET = "\033[0m"
 
-class HeartbeatNode(Node):
+class Heartbeat(Node):
     def __init__(self):
-        super().__init__('heartbeat_node')
+        super().__init__('heartbeat')
         self.publisher_ = self.create_publisher(String, 'heartbeat', 10)
         self.show_heart = True
-        self.timer = self.create_timer(0.5, self.timer_callback)  # 2 Hz
+        self.interval = 0.8  # seconds per beat
+        self.bpm = 60 / self.interval  # calculate beats per minute
+        self.timer = self.create_timer(self.interval, self.timer_callback)
 
     def timer_callback(self):
         if self.show_heart:
-            # Show heart and red text
-            print(f"\r{'❤️':<3} {RED}Heartbeat{RESET}", end="", flush=True)
+            # Show heart and red text with bpm
+            print(f"\r{'❤️':<3} {RED}Heartbeat at Rate: {self.bpm:.0f} BPM{RESET}", end="", flush=True)
+            # Beep sound
+            print("\a", end="", flush=True)  # ASCII bell
         else:
-            # Heart disappears, clear line (keep text blank)
-            print(f"\r{'':<2} {RED}Heartbeat{RESET}", end="", flush=True)  # 2 Hz
+            # Heart disappears, text remains
+            print(f"\r{'':<2} {RED}Heartbeat at Rate: {self.bpm:.0f} BPM{RESET}", end="", flush=True)
 
         self.show_heart = not self.show_heart  # toggle for next beat
 
@@ -30,11 +35,11 @@ class HeartbeatNode(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = HeartbeatNode()
+    node = Heartbeat()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
-        print("\nHeartbeat node stopped.")
+        print("\nHeartbeat stopped.")
     node.destroy_node()
     rclpy.shutdown()
 
